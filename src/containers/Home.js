6485a895d38e87d6209d6dc8e4deec79f25ae48e
Manoj Alaghappan <manoj_alaghappan@yahoo.com>
2019-10-15 17:20:49 +1000

    hook support in Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import "./Home.css";
 
-export default class Home extends Component {
-    render() {
-        return (
+function Home() {
+    return (
+        <div>
             <div className="Home">
                 <h1>Your Brand</h1>
                 <p>Your Brand description</p>
             </div>
-        );
-    }
+        </div>
+    )
 }
+
+export default Home
